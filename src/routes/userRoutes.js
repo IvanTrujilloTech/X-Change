83596f111ce413cc74ce36011370b21f858a603f
mariaandoli <mariaandoli@campus.monlau.com>
2025-11-24 17:19:22 +0100

    .

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -26,5 +26,27 @@ router.get('/:id', authenticateToken, (req, res) => {
     requestedBy: req.user.username
   });
 });
+router.put('/perfiles/:id', authenticateToken, async (req, res) => {
+  const userId = req.user.sub;
+  const { nombre, apellido1, apellido2, telefono, curso } = req.body;
+    res.json({ success: true, message: 'Perfil actualizado' });
+});
 
+router.get('/profile', authenticateToken, async (req, res) => {
+  const userId = req.user.sub; // ID del token
+    const profile = {
+    id_usuario: userId,
+    email: req.user.email,
+    id_rol: req.user.id_rol,
+    // Datos de la tabla 'perfiles':
+    nombre: 'María',
+    apellido1: 'García',
+    apellido2: 'López',
+    telefono: '123456789',
+    curso: '2DAW',
+    email_tutor: 'tutor@test.com'
+  };
+  
+  res.json({ success: true, profile });
+});
 export default router;
